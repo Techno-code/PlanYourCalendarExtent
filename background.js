825b621948384d6c9c4911692ab96cafe78348c5
Sean Yang <66633479+Techno-code@users.noreply.github.com>
2022-06-26 03:27:16 +0000

    new meta?

--- background.js
+++ background.js
@@ -4,3 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({ color });
 	console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+var todayday = new Date();
+var todayday = today.getDate();
+document.getElementById("today").innerHTML = 
