f137e39c3a36e9b7ca3caebe33113c8510a7089e
Ashwat Krishnamoorthy <34427744+ygdflgdflop@users.noreply.github.com>
2022-06-26 13:58:09 +0000

    FINAL

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function addEvent(number) {
     p.append(text);
     id.appendChild(p);
 }
+
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({'url': "chrome://newtab"})
+})
